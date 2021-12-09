dcb2c1f91482e382f02fb076a61ee782830d9b31
dil97 <94825500+dil97@users.noreply.github.com>
2021-12-09 12:46:54 +0530

    9 Dec 2021
    now today we will learn Clouse

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-// import "App.css";
 import React from "react";
-import { bikesFilter } from "./components/classcomp/bikesFilter";
-// import { Counter } from "./Services/Counter";
-// import { Bikeslist } from "./components/classcomp/Bikeslist";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
-// import {Navigation } from "./components/Navigation";
-// import {Home} from "./components/Home";
-// import{Footer} from "./components/Footer";
-// import { Cards } from "./components/Cards";
-// import ClassComp from "./components/classcomp/Classcomp";
-// import { Product$list } from "./components/classcomp/Product$list";
+import Countern, { Counter } from "./Services/Counter";
+import Navigation  from "./components/Navigation";
+import {Home} from "./components/Home";
+import{Footer} from "./components/Footer";
+import { Cards } from "./components/Cards";
+import Classcomp from"./components/classcomp/Classcomp"
+
+import { Product$list } from "./components/classcomp/Product$list";
 
 
+// import "App.css";
 
 
 const App = () => {
   return (
-    <div  >
-      <bikesFilter/>
+    <BrowserRouter>
+    <Navigation/>
+    <Routes>
+      <Route path="/"element={<Cards/>}/>
+      <Route path="/Prodact"element ={<Product$list/>}/>
+      <Route path="/Calculater"element={<Counter/>}/>
+        
+    </Routes>
+   
+    </BrowserRouter>
 
-      {/* <Counter/> */}
-      {/* <Bikeslist/> */}
-    
-      {/* <ClassComp/> */}
-      {/* <Product$list/> */}
-       {/* <Cards/>
-        <Home/>
-        <Footer/>
-        <Navigation/>  */}
 
-     
-      
-    </div>
   );
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,13 +5,13 @@ export const Navigation = () => {
         <tr className="nav">
             {/* <button  varriant ="danger">Click here</button> */}
             <td>
-                <a href="https://pickupbiz.com" target="_blank" > PickupBiz</a>
+                <link href="/Counter" tlinkrget="_bllinknk" >Calculator</link>
             </td>
             <td>
-                <a href="https://Amazon.com" target="_blank">Amazon</a>
-            </td>
+                <a href="/Product$list" target="_blank">product</a>
+            </td>           
             <td>
-                <a href="https://Google.com" target="_blank"> Google gmail</a>
+                <a href="/" target="_blank">Cards
             </td>
             <form >
                <h3> first Name</h3>
